91e24abb18191e78658f51039495807359ab7a3f
toxidxd <toxid13@yandex.ru>
2024-01-24 23:32:46 +0300

    .env added

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from dotenv import load_dotenv
 
 s = Service(executable_path='C:\\Users\\toxidxd\\PycharmProjects\\selenium_vk\\chromedriver-win64\\chromedriver.exe')
 driver = webdriver.Chrome(service=s)
+load_dotenv()
 
 try:
     driver.maximize_window()
@@ -15,7 +17,7 @@ try:
 
     email_input = driver.find_element(By.ID, 'index_email')
     email_input.clear()
-    email_input.send_keys('79381559123')
+    email_input.send_keys(os.getenv('PHONE'))
     email_input.send_keys(Keys.ENTER)
     time.sleep(10)
 
